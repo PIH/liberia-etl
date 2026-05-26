c8e3e8a20cab53ee22414a3c7c2c13287a7daf19
dmdesimone <ddesimone@pih.org>
2026-05-26 11:34:40 -0400

    LIB-399 fix all_encounters index

--- jobs/sql/derivations/update_index.sql
+++ jobs/sql/derivations/update_index.sql
@@ -1,8 +1,8 @@
 -- update index asc/desc on all_encounters table
 drop table if exists #derived_indexes;
 select  encounter_id,
-        ROW_NUMBER() over (PARTITION by patient_id, encounter_type_id order by encounter_datetime, encounter_id) as index_asc,
-        ROW_NUMBER() over (PARTITION by patient_id, encounter_type_id order by encounter_datetime DESC, encounter_id DESC) as index_desc
+        ROW_NUMBER() over (PARTITION by patient_id, encounter_type order by encounter_datetime, encounter_id) as index_asc,
+        ROW_NUMBER() over (PARTITION by patient_id, encounter_type order by encounter_datetime DESC, encounter_id DESC) as index_desc
 into    #derived_indexes
 from    all_encounters;
 
