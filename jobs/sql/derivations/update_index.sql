cf6c81f32266badbb0409262172216163e8e6fe0
dmdesimone <ddesimone@pih.org>
2026-05-26 13:54:27 -0400

    LIB-401-all_diagnoses-table
    Added logic to update index ascending and descending values in all_diagnoses table using derived indexes.

--- jobs/sql/derivations/update_index.sql
+++ jobs/sql/derivations/update_index.sql
@@ -11,3 +11,16 @@ set t.index_asc = i.index_asc,
 	t.index_desc = i.index_desc
 from all_encounters t inner join #derived_indexes i on i.encounter_id = t.encounter_id
 ;
+-- update patient  index asc/desc on all_diagnoses table
+drop table if exists #derived_indexes;
+select  obs_id,
+        ROW_NUMBER() over (PARTITION by patient_id order by obs_datetime, obs_id) as index_asc,
+        ROW_NUMBER() over (PARTITION by patient_id order by obs_datetime DESC, obs_id DESC) as index_desc
+into    #derived_indexes
+from    all_diagnoses;
+
+update t
+set t.index_asc = i.index_asc,
+    t.index_desc = i.index_desc
+from all_diagnoses t inner join #derived_indexes i on i.obs_id = t.obs_id
+;
