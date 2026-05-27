592cdaa7977006cbe89ac26a956c529f2005a9d6
dmdesimone <ddesimone@pih.org>
2026-05-27 13:55:25 -0400

    LIB-405-new-all_programs-table

---
+++ jobs/sql/schemas/all_programs.sql
@@ -0,0 +1,13 @@
+create table all_programs
+(
+  patient_program_id    varchar(50),
+  patient_id            varchar(50),
+  emr_id                varchar(50),
+  program_name          varchar(50),
+  date_enrolled         date,
+  date_completed        date,
+  program_outcome       varchar(255),
+  user_entered          varchar(50),
+  index_asc             int,
+  index_desc            int  
+);
